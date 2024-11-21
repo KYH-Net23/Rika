c925b204807772ca0c4aabf4323deb67e408c1d2
Nant1ng <andressantana99@hotmail.se>
2024-11-21 14:00:09 +0100

    Fixed a useEffect

--- Rika/src/views/sections/products/checkout/OrderSummary.jsx
+++ Rika/src/views/sections/products/checkout/OrderSummary.jsx
@@ -1,28 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import ArrowBack from '../../../../common/ArrowBack'
+import { useEffect, useState } from "react";
+import ArrowBack from "../../../../common/ArrowBack";
 
-const OrderSummary = ({slideNumber, clickNextFunc, clickPrevFunc}) => {
-    const [cartItem, setCartItem] = useState([])
+const OrderSummary = ({ slideNumber, clickNextFunc, clickPrevFunc }) => {
+  const [cartData, setCartData] = useState([]);
+  const [customerData, setCustomerData] = useState([]);
+  const [shippingData, setShippingData] = useState([]);
+  const [paymentData, setPaymentData] = useState([{ paymentType: "Stripe" }]);
 
-    useEffect(()=>{
-        const cartItems = JSON.parse(localStorage.getItem("cartItems"))
-    })
+  const fetchLocalStorage = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems"));
+    const customerInfo = JSON.parse(localStorage.getItem("customerInfo"));
+    const shippingInfo = JSON.parse(
+      localStorage.getItem("shippingInformation")
+    );
+
+    setCartData(cartItems);
+    setCustomerData(customerInfo);
+    setShippingData(shippingInfo);
+  };
+
+  useEffect(() => {
+    fetchLocalStorage();
+  });
+
   return (
-          <>
+    <>
       <ArrowBack clickFunc={clickPrevFunc} />
       <section className="flex flex-col min-w-[355px] md:min-w-[562px]">
         <h3 className="font-mont font-semibold">
           <span className="bg-[#CCC] py-1 px-[10px] rounded-full mr-2">
             {slideNumber}
           </span>
-          Order Summary
+          {cartData.map((product) => (
+            <h1>{product.brand}</h1>
+          ))}
+          <h2>{paymentData[0].paymentType}</h2>
         </h3>
-        <div>
-            
-        </div>
-        </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default OrderSummary
+export default OrderSummary;
