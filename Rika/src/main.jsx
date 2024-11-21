534aaba7b9c8cb2470087c2d371db2caef137202
TuyaNarangerel <146025683+TuyaNarangerel@users.noreply.github.com>
2024-11-21 07:35:20 +0100

    Fixat main.jsx med korrekta rutter och löst konflikter

--- Rika/src/main.jsx
+++ Rika/src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "./lib/AuthProvider.jsx";
 import ProtectedRoute from "./lib/ProtectedRoute.jsx";
 import { ProductProvider } from "./lib/ProductProvider.jsx";
 import { ShippingProvider } from "./lib/ShippingOptionsProvider.jsx";
+import { PaymentProvider } from "./lib/PaymentProvider.jsx";
 
 import "./assets/css/main.css";
 
@@ -22,16 +23,9 @@ import CreateProduct from "./views/CreateProduct";
 import Users from "./views/Users";
 import RedirectToPaymentForm from "./views/payment/RedirectToPaymentForm.jsx";
 import Register from "./views/Register.jsx";
-<<<<<<< HEAD
 import AllInvoices from "./views/Invoice/AllInvoices.jsx";
-import ShippingOptions from "./views/Checkout/ShippingOptions.jsx";
-=======
-import AllInvoices from "./views/AllInvoices";
-
-import ShippingOptions from "./views/shipping/ShippingOptions.jsx";
-import { PaymentProvider } from "./lib/PaymentProvider.jsx";
 import ProductReturnPage from "./views/customerpages/Orders/Returns/ReturnPage.jsx";
->>>>>>> origin/dev
+import ShippingOptions from "./views/shipping/ShippingOptions.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -39,58 +33,22 @@ createRoot(document.getElementById("root")).render(
       <AuthProvider>
         <ProductProvider>
           <ShippingProvider>
-<<<<<<< HEAD
-            <Header />
-            <div className="px-4 pt-10 pb-[86px]">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/productdetails/:id" element={<ProductDetails />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/productscreate" element={<CreateProduct />} />
-                <Route path="/admin/edit-product/:id" element={<EditProduct />} />
-                <Route path="/users" element={<Users />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/paymentformtest" element={<RedirectToPaymentForm />} />
-                <Route path="/shipping" element={<ShippingOptions />} />
-                <Route path="/return" element={<ReturnFromPayment />} />
-                <Route path="/edit-invoice/:id" element={<EditInvoice />} />
-
-                <Route
-                  path="/customer"
-                  element={
-                    <ProtectedRoute requiredRole="customer">
-                      <CustomerLandingPage />
-                    </ProtectedRoute>
-                  }
-=======
             <PaymentProvider>
-
               <Header />
               <div className="px-4 pt-10 pb-[86px]">
                 <Routes>
-                  <Route path="/productReturn" element={<ProductReturnPage />} /> 
                   <Route path="/" element={<Home />} />
                   <Route path="/products" element={<Products />} />
-                  <Route
-                  path="/productdetails/:id"
-                  element={<ProductDetails />}
->>>>>>> origin/dev
-                />
+                  <Route path="/productdetails/:id" element={<ProductDetails />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/productscreate" element={<CreateProduct />} />
-                  <Route
-                  path="/admin/edit-product/:id"
-                  element={<EditProduct />}
-                />
+                  <Route path="/admin/edit-product/:id" element={<EditProduct />} />
                   <Route path="/users" element={<Users />} />
                   <Route path="/register" element={<Register />} />
-                  <Route
-                  path="/paymentformtest"
-                  element={<RedirectToPaymentForm />}
-                />
+                  <Route path="/paymentformtest" element={<RedirectToPaymentForm />} />
                   <Route path="/shipping" element={<ShippingOptions />} />
                   <Route path="/return" element={<ReturnFromPayment />} />
+                  <Route path="/productReturn" element={<ProductReturnPage />} />
                   <Route
                     path="/customer"
                     element={
