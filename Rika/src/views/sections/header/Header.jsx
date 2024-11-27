ed6bb5a8fa35ec69f0c8b89e44d8b9692d27f817
Mille Elfver <mille.elfver98@gmail.com>
2024-11-27 09:46:37 +0100

    fixed navigate to cart button

--- Rika/src/views/sections/header/Header.jsx
+++ Rika/src/views/sections/header/Header.jsx
@@ -12,6 +12,10 @@ const Header = () => {
     navigate("/");
   };
 
+  const handleCartClick = () => {
+    navigate("/checkout");
+  };
+
   return (
     <header className="fixed bottom-0 flex bg-white w-full min-h-[70px] rounded-[30px_30px_0px_0px] shadow-[0px_-2px_7px_rgba(0,0,0,0.1)] z-50">
       <div className="flex gap-14 w-full items-center justify-center">
@@ -23,9 +27,11 @@ const Header = () => {
           <p className="font-mont text-[11px] font-semibold">Home</p>
         </button>
         <nav className="flex items-center gap-14">
-          <a className="cursor-pointer">
-            <CartIcon />
-          </a>
+          <button onClick={handleCartClick}>
+            <a className="cursor-pointer">
+              <CartIcon />
+            </a>
+          </button>
           <a className="cursor-pointer">
             <NotificationIcon />
           </a>
