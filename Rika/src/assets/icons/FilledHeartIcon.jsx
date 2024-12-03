f0e054f9b04933480592da85c8afad9d5183476e
Didi <demi.torrealba@hotmail.com>
2024-12-03 21:45:13 +0100

    icons

---
+++ Rika/src/assets/icons/FilledHeartIcon.jsx
@@ -0,0 +1,21 @@
+const FilledHeartIcon = () => (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 24 24" 
+      fill="black" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 21s-1.74-1.21-3.28-2.72C6.71 16.23 4 13.37 4 10a4 4 0 0 1 8-1 4 4 0 0 1 8 1c0 3.37-2.71 6.23-4.72 8.28C13.74 19.79 12 21 12 21z"
+        fill="black" 
+        stroke="black" 
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+  
+  export default FilledHeartIcon;
